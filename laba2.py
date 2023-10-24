258ff5c45c5ce17ae39489549520454dd8a3bafb
Gavarin23 <146112306+Gavarin23@users.noreply.github.com>
2023-10-24 09:44:53 +0300

    laba2.py

--- laba2.py
+++ laba2.py
@@ -32,3 +32,36 @@ def multi_friends_joke(friend: str, jokes_number: int):
         result += friend + f" tells his joke #{i + 1}: " + pyjokes.get_joke() + " "
     return result
 
+from  fastapi import FastAPI
+import pyjokes
+from pydantic import BaseModel
+
+app = FastAPI()
+
+from  fastapi import FastAPI
+import pyjokes
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get('/')
+def kalkulator(value1: int, value2: int, znak: str):
+    if (znak == '+'):
+        return value1 + value2
+
+    if (znak == '-'):
+        return value1 - value2
+
+    if (znak == '*'):
+        return value1 * value2
+
+    if (znak == '/') and (value2 == 0):
+        return 'АШИБКА'
+
+    if (znak == '/'):
+        return value1 / value2
+
+    if (znak == '**'):
+        return value1 ** value2
+
